f55ee5b22821bd39678d8977002b7f0f92adc3a9
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2022-10-19 01:03:21 +0300

    Add files via upload
    Initial commit

---
+++ rush00/main.c
@@ -0,0 +1,7 @@
+void	rush(int column, int line);
+
+int	main(void)
+{
+	rush(5, 5);
+	return (0);
+}
---
+++ rush00/rush01.c
@@ -0,0 +1,49 @@
+void	ft_putchar(char c);
+
+void	print(int len, char begin, char mid, char last)
+{
+	int	column_count;
+
+	column_count = 1;
+	while (column_count <= len)
+	{
+		if (column_count == 1)
+		{
+			ft_putchar(begin);
+		}
+		else if (column_count == len)
+		{
+			ft_putchar(last);
+		}
+		else
+			ft_putchar(mid);
+		column_count += 1;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int column, int line)
+{
+	int	line_count;
+
+	line_count = 1;
+	if (column >= 1 && line >= 1)
+	{
+		while (line_count <= line)
+		{
+			if (line_count == 1)
+			{
+				print(column, '/', '*', '\\');
+			}
+			else if (line_count == line)
+			{
+				print(column, '\\', '*', '/');
+			}
+			else
+			{
+				print(column, '*', ' ', '*');
+			}
+			line_count++;
+		}
+	}
+}
