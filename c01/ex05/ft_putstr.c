f5555a3d63183ba84d99157888cdf92aa1b7dfbd
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2022-10-20 18:03:47 +0300

    Add files via upload
    c01 finish

---
+++ c01/ex05/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: onigiz <onigiz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/18 15:22:08 by onigiz            #+#    #+#             */
+/*   Updated: 2022/10/18 15:22:10 by onigiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char x)
+{
+	write(1, &x, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		ft_putchar(str[counter]);
+		counter++;
+	}
+}
---
+++ c01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: onigiz <onigiz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 14:06:43 by onigiz            #+#    #+#             */
+/*   Updated: 2022/10/20 14:37:53 by onigiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	swap;
+
+	i = 0;
+	while (i < (size / 2))
+	{
+		swap = tab[i];
+		tab[i] = tab[size - 1 - i];
+		tab[size - 1 - i] = swap;
+		i++;
+	}
+}
